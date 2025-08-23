5a3eecb60a5a099e401b5b35fcb850d7a11efa46
JVitorRodrigues <joaovitorrs2017@gmail.com>
2025-08-23 10:37:38 -0300

    Add files via upload

---
+++ # Atividade de programação em Python.py
@@ -0,0 +1,12 @@
+# Atividade de programação em Python 
+# Faça um programa que leia dois valores e troque seus conteudos sem usar uma teceira variável 
+#e usando operadores aritméticos.
+
+A=20
+B=5
+
+A= A + B 
+B= B * A
+
+print ("A = ", A)
+print ("B = ", B)
