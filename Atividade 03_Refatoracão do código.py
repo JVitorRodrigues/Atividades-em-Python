67cd756e0c6f1db739f727547838c86f54a57a89
JVitorRodrigues <joaovitorrs2017@gmail.com>
2025-08-25 21:12:18 -0300

    Add files via upload

---
+++ Atividade 03_Refatoracão do código.py
@@ -0,0 +1,30 @@
+#Refatoracão do código
+
+# Reescreva o código à seguir aplicando boas práticas:
+# Nomes de variáveis descritivos.
+#   Indentação correta.
+#   Comentários explicativos.
+#   Espaçamento adequado.
+
+#           Código:
+# a=10
+# b=20
+# if a>b:
+# print("a maior")
+# else:
+# print("b maior")
+
+("-----------------------------------------------------------")
+
+# Definindo os valores para as variáveis
+numero_a = 10
+numero_b = 20
+
+# Comparando os valores das variáveis
+if numero_a > numero_b:
+# Caso o valor de 'numero_a' seja maior
+    print("O número 'a' é maior")
+else:
+# Caso contrário, o valor de 'numero_b' é maior
+    print("O número 'b' é maior")
+
